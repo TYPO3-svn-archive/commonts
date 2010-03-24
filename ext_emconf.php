caaf3c6897519ba051057c1c46222c2e6dc414ec
1stmachine <1stmachine@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-24 12:41:32 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commonts/trunk@31436 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "commonts".
+#
+# Auto generated 08-03-2010 20:15
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Common TS',
+	'description' => 'library of typoscript object to get your site quickly up and runing',
+	'category' => 'fe',
+	'author' => 'Christian Wolff',
+	'author_email' => 'chris@connye.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4439";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"36d8";}',
+);
+
+?>
