af5dafe3c9fd2aa246611540eb417fe7190beb0a
1stmachine <1stmachine@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-30 13:42:41 +0000

    add extension icon some changes in setup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/commonts/trunk@31734 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "commonts".
 #
-# Auto generated 08-03-2010 20:15
+# Auto generated 24-03-2010 14:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4439";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"36d8";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"4439";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"10f7";s:14:"doc/manual.sxw";s:4:"e556";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"36d8";s:30:"static/international/setup.txt";s:4:"64ba";s:32:"static/xhtml_trans/constants.txt";s:4:"d41d";s:28:"static/xhtml_trans/setup.txt";s:4:"6965";}',
+	'suggests' => array(
+	),
 );
 
 ?>
